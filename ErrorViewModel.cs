63a3e21b27d2cdffe9572806e0ace22bb42c0dba
maurymaee <maurymae06@gmail.com>
2026-01-18 13:44:58 +0800

    Add files via upload

---
+++ ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AppDev1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ HomeController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleMvcSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
